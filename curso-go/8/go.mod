1d0ec019eb8163525fc0239f7c0af995f695f3d5
jb-oliveira <jbacos77@gmail.com>
2025-10-25 19:07:52 -0300

    feat: add lessons

---
+++ curso-go/8/go.mod
@@ -0,0 +1,8 @@
+module github.com/jb-oliveira/fullcycle/tree/main/curso-go/8
+
+go 1.25.3
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/exp v0.0.0-20251023183803-a4bb9ffd2546 // indirect
+)
--- curso-go/8/main.go
+++ curso-go/8/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	mat "github.com/jb-oliveira/fullcycle/tree/main/curso-go/8/matematica"
+)
+
 func main() {
 
+	s := mat.Soma(1, 2)
+	println(s)
+	val, err := uuid.NewV7()
+	if err != nil {
+		println(err)
+
+	} else {
+		fmt.Printf("Valor = %v", val)
+	}
+
+	for i := 0; i < 10; i++ {
+		println(i)
+	}
+
+	slc := []string{"A", "B", "C"}
+	for k, v := range slc {
+		println(k, v)
+	}
+
+	i := 0
+	for i < 10 {
+		println(i)
+		i++
+	}
+
+	// println(val)
 }
