e7540c374fbbc015c675dd6714db96e8a112a79d
jb-oliveira <jbacos77@gmail.com>
2025-11-05 17:52:12 -0300

    organize lessons

---
+++ curso-go/initial/server-mux/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomeHandler)
+	mux.Handle("/blog", Blog{Title: "My Blog"})
+	http.ListenAndServe(":8080", mux)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Ola mmundo"))
+}
+
+type Blog struct {
+	Title string
+}
+
+func (b Blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(b.Title))
+}
