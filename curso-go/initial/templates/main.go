854fdac325ce0a3909c6b407d6ada207608fde79
jb-oliveira <jbacos77@gmail.com>
2025-11-08 16:55:18 -0300

    feat: templates

--- curso-go/initial/templates/main.go
+++ curso-go/initial/templates/main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"log"
+	"os"
+	"strings"
+)
 
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func MyUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+func main() {
+	// curso := Curso{"GO", 40}
+	// tmp := template.New("CursoTemplate")
+	// tmp, err := tmp.Parse("Curso: {{.Nome}} - Carga Horaria {{.CargaHoraria}}")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// err = tmp.Execute(os.Stdout, curso)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	templates := []string{
+		"header.html",
+		"content.html",
+		"footer.html",
+	}
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	t := template.Must(template.New("content.html").ParseFiles(templates...))
+	// 	err := t.Execute(w, Cursos{
+	// 		{"GO", 40},
+	// 		{"Java", 40},
+	// 		{"Python", 50},
+	// 	})
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// })
+	// http.ListenAndServe(":8080", nil)
+
+	t := template.New("content.html")
+	t.Funcs(template.FuncMap{"MyUpper": MyUpper})
+	t = template.Must(t.ParseFiles(templates...))
+	err := t.Execute(os.Stdout, Cursos{
+		{"GO", 40},
+		{"Java", 40},
+		{"Python", 50},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
