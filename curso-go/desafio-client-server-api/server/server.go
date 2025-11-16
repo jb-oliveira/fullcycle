28e66eb76dd121b3ae69de6263d02ab97999b78e
jb-oliveira <jbacos77@gmail.com>
2025-11-16 16:57:36 -0300

    feat: server go initial draft

--- curso-go/desafio-client-server-api/server/server.go
+++ curso-go/desafio-client-server-api/server/server.go
@@ -1,5 +1,107 @@
 package main
 
-func main() {
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
 
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	appDB *gorm.DB
+)
+
+type USDBRL struct {
+	gorm.Model
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+}
+
+type UrlRequest struct {
+	USDBRL USDBRL `json:"USDBRL"`
+}
+
+type UrlResponse struct {
+	Bid string `json:"bid"`
+}
+
+func BuscaDolarHandler(w http.ResponseWriter, r *http.Request) {
+	request, err := loadDataFromUrl()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = saveToDataBase(request)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	result := UrlResponse{
+		Bid: request.USDBRL.Bid,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
+}
+
+func saveToDataBase(entity *UrlRequest) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+	return gorm.G[USDBRL](appDB).Create(ctx, &entity.USDBRL)
+}
+
+func loadDataFromUrl() (*UrlRequest, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+	res, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result UrlRequest
+	err = json.Unmarshal(body, &result)
+	return &result, nil
+}
+
+func InitializeSQLite() {
+	fmt.Println("Initializing SQLite database")
+
+	appDB, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = appDB.AutoMigrate(&USDBRL{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("SQLite database initialized")
+
+}
+
+func main() {
+	InitializeSQLite()
+	http.HandleFunc("/", BuscaDolarHandler)
+	http.ListenAndServe(":8080", nil)
 }
