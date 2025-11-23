1207119c1a4ee5b1c8950d583948c6e2020c11c8
jb-oliveira <jbacos77@gmail.com>
2025-11-23 08:48:06 -0300

    feat: add configs

--- curso-go/9-APIS/cmd/server/main.go
+++ curso-go/9-APIS/cmd/server/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/jb-oliveira/fullcycle/tree/main/curso-go/9-APIS/configs"
+)
+
+func main() {
+	dbConf, _ := configs.LoadDbConfig(".")
+	fmt.Printf("DBConfig: %v", &dbConf)
+	webConf, _ := configs.LoadWebConfig(".")
+	fmt.Printf("WebConfig: %v", &webConf)
 }
---
+++ curso-go/9-APIS/configs/config.go
@@ -0,0 +1,73 @@
+package configs
+
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+var (
+	dbConfig  *confDB
+	webConfig *confWeb
+)
+
+type confDB struct {
+	DBDriver   string `mapstructure:"DB_DRIVER"`
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBName     string `mapstructure:"DB_NAME"`
+}
+
+type confWeb struct {
+	WebServerPort string `mapstructure:"WEB_PORT"`
+	JWTSecret     string `mapstructure:"JWT_SECRET"`
+	JWTExpiration int    `mapstructure:"JWT_EXPIRATION"`
+	TokenAuth     *jwtauth.JWTAuth
+}
+
+func LoadDbConfig(path string) (*confDB, error) {
+	viper.SetConfigName("db_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	// viper.AutomaticEnv() = Isso faz com que ele sobscreva as variaveis de ambiente
+	// ao inves do que tiver no .env  Caso  não tenha nas variaveis de ambiente ele carrega o que ta .env
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	err = viper.Unmarshal(&dbConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	return dbConfig, err
+}
+
+func LoadWebConfig(path string) (*confWeb, error) {
+	viper.SetConfigName("web_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	// viper.AutomaticEnv() = Isso faz com que ele sobscreva as variaveis de ambiente
+	// ao inves do que tiver no .env  Caso  não tenha nas variaveis de ambiente ele carrega o que ta .env
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	err = viper.Unmarshal(&webConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	webConfig.TokenAuth = jwtauth.New("HS256", []byte(webConfig.JWTSecret), nil)
+
+	return webConfig, err
+}
