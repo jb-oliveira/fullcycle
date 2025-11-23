bc3e8ccb655fb95acf6c76fe99d628272892ad6f
jb-oliveira <jbacos77@gmail.com>
2025-11-23 10:10:50 -0300

    Moving directory

---
+++ APIS/cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import _ "github.com/jb-oliveira/fullcycle/tree/main/curso-go/9-APIS/configs" // used to execute init() method in config
+
+func main() {
+
+}
---
+++ APIS/internal/entity/user.go
@@ -0,0 +1,31 @@
+package entity
+
+import (
+	"github.com/jb-oliveira/fullcycle/tree/main/curso-go/9-APIS/pkg/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"-"`
+}
+
+func (u *User) ValidatePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       entity.NewID(),
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+	}, nil
+}
