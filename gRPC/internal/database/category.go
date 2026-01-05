c1ed91a2db043b186bcde504049403df8639f18e
jb-oliveira <jbacos77@gmail.com>
2026-01-05 20:20:18 -0300

    gRPC initial commit 2

---
+++ gRPC/internal/database/category.go
@@ -0,0 +1,65 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type Category struct {
+	db          *sql.DB
+	Id          string
+	Name        string
+	Description string
+}
+
+func NewCategory(db *sql.DB) *Category {
+	return &Category{db: db}
+}
+
+func (c *Category) Create() error {
+	stmt, err := c.db.Prepare("INSERT INTO categories (id, name, description) VALUES ($1, $2, $3)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	uuid := uuid.Must(uuid.NewV7()).String()
+	c.Id = uuid
+	_, err = stmt.Exec(c.Id, c.Name, c.Description)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Category) FindAll() ([]Category, error) {
+	rows, err := c.db.Query("SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var categories []Category
+	for rows.Next() {
+		var category Category
+		err := rows.Scan(&category.Id, &category.Name, &category.Description)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+
+	return categories, nil
+}
+
+func (c *Category) FindByCourseID(courseID string) (Category, error) {
+	var category Category
+	row := c.db.QueryRow("SELECT c.id, c.name, c.description FROM categories c JOIN courses cc ON c.id = cc.category_id WHERE cc.id = $1", courseID)
+	err := row.Scan(&category.Id, &category.Name, &category.Description)
+	if err != nil {
+		return Category{}, err
+	}
+	return category, nil
+}
---
+++ gRPC/internal/database/course.go
@@ -0,0 +1,73 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type Course struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+	CategoryID  string
+}
+
+func NewCourse(db *sql.DB) *Course {
+	return &Course{db: db}
+}
+
+func (c *Course) Create() error {
+	stmt, err := c.db.Prepare("INSERT INTO courses (id, name, description, category_id) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	id := uuid.Must(uuid.NewV7()).String()
+	c.ID = id
+	_, err = stmt.Exec(c.ID, c.Name, c.Description, c.CategoryID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Course) FindAll() ([]Course, error) {
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var courses []Course
+	for rows.Next() {
+		var course Course
+		err := rows.Scan(&course.ID, &course.Name, &course.Description, &course.CategoryID)
+		if err != nil {
+			return nil, err
+		}
+		courses = append(courses, course)
+	}
+	return courses, nil
+}
+
+func (c *Course) FindByCategoryID(categoryID string) ([]Course, error) {
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses WHERE category_id = ?", categoryID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var courses []Course
+	for rows.Next() {
+		var course Course
+		err := rows.Scan(&course.ID, &course.Name, &course.Description, &course.CategoryID)
+		if err != nil {
+			return nil, err
+		}
+		courses = append(courses, course)
+	}
+	return courses, nil
+}
