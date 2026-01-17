b174521bef904914745a746fd2324d7ca6d9be7a
jb-oliveira <jbacos77@gmail.com>
2026-01-17 08:46:09 -0300

    after antyigravity

--- gRPC/internal/service/category_client.go
+++ gRPC/internal/service/category_client.go
@@ -1,39 +1,43 @@
+package service
+
 import (
 	"context"
 	"io"
 	"log"
 	"time"
 
+	"github.com/jb-oliveira/fullcycle/gRPC/internal/database"
+	"github.com/jb-oliveira/fullcycle/gRPC/internal/pb"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type CategoryClient struct {
-	Client CategoryServiceClient
+	Client pb.CategoryServiceClient
 }
 
-func NewCategoryClient(client CategoryServiceClient) *CategoryClient {
+func NewCategoryClient(client pb.CategoryServiceClient) *CategoryClient {
 	return &CategoryClient{Client: client}
 }
 
-func (s *CategoryClient) Create(name string, desc string) (*CategoryResponse, error) {
+func (s *CategoryClient) Create(name string, desc string) (*pb.CategoryResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 
-	return s.Client.CreateCategory(ctx, &CreateCategoryRequest{Name: name, Description: desc})
+	return s.Client.CreateCategory(ctx, &pb.CreateCategoryRequest{Name: name, Description: desc})
 }
 
-func (s *CategoryClient) List() (*CategoryListResponse, error) {
+func (s *CategoryClient) List() (*pb.CategoryListResponse, error) {
 	return s.Client.ListCategory(context.Background(), &emptypb.Empty{})
 }
 
-func (s *CategoryClient) CreateBulk(categoryDB []database.CategoryDB) (*CategoryListResponse, error) {
+func (s *CategoryClient) CreateBulk(categoryDB []database.CategoryDB) (*pb.CategoryListResponse, error) {
 	stream, err := s.Client.CreateCategoryStream(context.Background())
 	if err != nil {
 		return nil, err
 	}
 
 	for _, category := range categoryDB {
-		if err := stream.Send(&CreateCategoryRequest{Name: category.Name, Description: category.Description}); err != nil {
+		if err := stream.Send(&pb.CreateCategoryRequest{Name: category.Name, Description: category.Description}); err != nil {
 			return nil, err
 		}
 	}
@@ -51,27 +55,31 @@ func (s *CategoryClient) CreateChatty(categoryDB []database.CategoryDB) error {
 	waitc := make(chan struct{})
 
 	// Receive goroutine
-	go func() {
-		for {
-			res, err := stream.Recv()
-			if err == io.EOF {
-				close(waitc)
-				return
-			}
-			if err != nil {
-				return err
-			}
-			log.Printf("Server processed: %s", res.Name)
-		}
-	}()
+	go s.receive(stream, waitc)
 
 	// Send messages
 	for _, category := range categoryDB {
-		if err := stream.Send(&CreateCategoryRequest{Name: category.Name, Description: category.Description}); err != nil {
+		if err := stream.Send(&pb.CreateCategoryRequest{Name: category.Name, Description: category.Description}); err != nil {
 			return err
 		}
 	}
 	stream.CloseSend()
 	<-waitc
 	return nil
+}
+
+func (s *CategoryClient) receive(stream pb.CategoryService_CreateCategoryStreamBidirectionalClient, waitc chan struct{}) {
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			close(waitc)
+			return
+		}
+		if err != nil {
+			log.Printf("Error receiving from stream: %v", err)
+			close(waitc)
+			return
+		}
+		log.Printf("Server processed: %s", res.Category.Name)
+	}
 }
