0b62ddbf7d4aec1e46c03733e3cbb3553beeeeda
jb-oliveira <jbacos77@gmail.com>
2026-02-07 09:38:05 -0300

    making more legible

--- DI/product/repository.go
+++ DI/product/repository.go
@@ -6,16 +6,16 @@ type ProductRepository interface {
 	Insert(product *Product) error
 }
 
-type ProductRepositoryPostgres struct {
-	DB *sql.DB
+type productRepositoryPostgres struct {
+	db *sql.DB
 }
 
 func NewProductRepositoryPostgres(db *sql.DB) ProductRepository {
-	return &ProductRepositoryPostgres{DB: db}
+	return &productRepositoryPostgres{db: db}
 }
 
-func (r *ProductRepositoryPostgres) Insert(product *Product) error {
-	_, err := r.DB.Exec("INSERT INTO products (id, name, price) VALUES ($1, $2, $3)", product.ID, product.Name, product.Price)
+func (r *productRepositoryPostgres) Insert(product *Product) error {
+	_, err := r.db.Exec("INSERT INTO products (id, name, price) VALUES ($1, $2, $3)", product.ID, product.Name, product.Price)
 	if err != nil {
 		return err
 	}
--- DI/product/usecase.go
+++ DI/product/usecase.go
@@ -10,16 +10,16 @@ type InsertProductUseCase interface {
 	Execute(product *ProductInputDto) error
 }
 
-type ProductUseCaseImpl struct {
-	ProductRepository ProductRepository
+type productUseCaseImpl struct {
+	productRepository ProductRepository
 }
 
 func NewProductUseCaseImpl(productRepository ProductRepository) InsertProductUseCase {
-	return &ProductUseCaseImpl{ProductRepository: productRepository}
+	return &productUseCaseImpl{productRepository: productRepository}
 }
 
-func (u *ProductUseCaseImpl) Execute(product *ProductInputDto) error {
-	err := u.ProductRepository.Insert(&Product{
+func (u *productUseCaseImpl) Execute(product *ProductInputDto) error {
+	err := u.productRepository.Insert(&Product{
 		ID:    product.ID,
 		Name:  product.Name,
 		Price: product.Price,
